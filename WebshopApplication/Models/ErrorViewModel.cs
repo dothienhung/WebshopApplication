6ff28ef2ef58f182cc4d049300afa07f3a861a58
dothienhung <jinxchn2702@gmail.com>
2020-06-20 16:45:33 +0700

    Add project files.

---
+++ WebshopApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebshopApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
